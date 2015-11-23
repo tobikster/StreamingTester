dab0ee510496ddf66e8e81defba6078438dbf0cb
tobikster <tobikster@gmail.com>
2015-11-23 17:42:14 +0100

    UI test - Espresso vs UIAutomator

--- app/src/androidTest/java/tobikster/streamingtester/TestCase.java
+++ app/src/androidTest/java/tobikster/streamingtester/TestCase.java
@@ -1,9 +1,14 @@
 package tobikster.streamingtester;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,10 +17,6 @@ import org.junit.runner.RunWith;
 
 import tobikster.streamingtester.activities.MainActivity;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by tobikster on 2015-11-15.
@@ -34,11 +35,30 @@ public class TestCase {
 	}
 
 	@Test
-	public void testBasicInterface() {
-		onView(withId(R.id.exo_player_test_button)).perform(click());
-		pressBack();
-		onView(withId(R.id.media_player_test_button)).perform(click());
-		pressBack();
-		onView(withId(R.id.web_view_test_button)).perform(click());
+	public void testBasicInterface() throws Exception {
+		UiObject exoPlayerButton = mDevice.findObject(new UiSelector().resourceId("tobikster.streamingtester:id/exo_player_test_button"));
+		UiObject mediaPlayerButton = mDevice.findObject(new UiSelector().resourceId("tobikster.streamingtester:id/media_player_test_button"));
+		UiObject webViewButton = mDevice.findObject(new UiSelector().resourceId("tobikster.streamingtester:id/web_view_test_button"));
+
+		for(int i = 0; i < 10; ++i) {
+			exoPlayerButton.click();
+			mDevice.pressBack();
+			mediaPlayerButton.click();
+			mDevice.pressBack();
+			webViewButton.click();
+			mDevice.pressBack();
+		}
+	}
+
+	@Test
+	public void testBasicInterface2() throws Exception {
+		for(int i = 0; i < 10; ++i) {
+			Espresso.onView(ViewMatchers.withId(R.id.exo_player_test_button)).perform(ViewActions.click());
+			Espresso.pressBack();
+			Espresso.onView(ViewMatchers.withId(R.id.media_player_test_button)).perform(ViewActions.click());
+			Espresso.pressBack();
+			Espresso.onView(ViewMatchers.withId(R.id.web_view_test_button)).perform(ViewActions.click());
+			Espresso.pressBack();
+		}
 	}
 }
